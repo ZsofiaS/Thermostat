ecadd0d2ba4e02009b44f96eed52040c5074a690
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-05-13 11:13:58 +0100

    Implement power saving mode

--- spec/thermostat_spec.js
+++ spec/thermostat_spec.js
@@ -1,3 +1,4 @@
+'use strict';
 describe('Thermostat', function() {
   var thermostat;
 
@@ -6,26 +7,67 @@ describe('Thermostat', function() {
   })
 
   it('has a default temperature of 20', function() {
-    expect(thermostat.temperature()).toEqual(20);
+    expect(thermostat.getTemperature()).toEqual(20);
   })
   it('has a minimum temperature of 10', function() {
-    for (var i = 1; i <= 10; i++) {
+    for (var i = 1; i <= 11; i++) {
       thermostat.down();
     }
-    expect(function() { thermostat.down(); }).toThrowError('Minimum temp reached');
+    expect(thermostat.getTemperature()).toEqual(10);
+  })
+  it('has the power saving mode on by default', function() {
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  })
+  it('can switch power saving mode off', function() {
+    thermostat.switchOff();
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+  })
+  it('can switch power saving mode on', function() {
+    thermostat.switchOff();
+    thermostat.switchOn();
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  })
+
+  describe('when power saving mode is on', function() {
+    it('has a max temp of 25', function() {
+      for (var i = 1; i <= 6; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.getTemperature()).toEqual(25);
+    })
+  })
+  describe('when power saving mode is off', function() {
+    it('has a max temp of 32', function() {
+      thermostat.switchOff();
+      for (var i = 1; i <= 13; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.getTemperature()).toEqual(32);
+    })
   })
 
   describe('#up', function() {
     it('can increase the temperature with an up function', function() {
       thermostat.up()
-      expect(thermostat.temperature()).toEqual(21);
+      expect(thermostat.getTemperature()).toEqual(21);
     })
   })
-
   describe('#down', function() {
     it('can decrease the temperature with a down function', function() {
       thermostat.down();
-      expect(thermostat.temperature()).toEqual(19);
+      expect(thermostat.getTemperature()).toEqual(19);
+    })
+  })
+  describe('#switchOn', function() {
+    it('sets max temp to 25', function() {
+      thermostat.switchOn();
+      expect(thermostat.getMaximumTemperature()).toEqual(25);
+    })
+  })
+  describe('#switchOff', function() {
+    it('sets max temp to 32', function() {
+      thermostat.switchOff();
+      expect(thermostat.getMaximumTemperature()).toEqual(32);
     })
   })
 })
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,17 +1,41 @@
+'use strict';
+
 class Thermostat {
   constructor() {
     this._temperature = 20;
+    this._MINTEMP = 10;
+    this._maxTemp = 25;
+    this._powerSavingModeOn = true;
   }
-  temperature() {
+  getTemperature() {
     return this._temperature;
   }
+  getMaximumTemperature() {
+    return this._maxTemp;
+  }
   up() {
+    if (this._temperature >= this._maxTemp) {
+      return;
+    }
     this._temperature++;
   }
   down() {
-    if (this._temperature <= 10) {
-      throw new Error('Minimum temp reached');
+    if (this._temperature <= this._MINTEMP) {
+      return;
     }
     this._temperature--;
+  }
+  isPowerSavingModeOn() {
+    return this._powerSavingModeOn === true;
+  }
+  switchOff() {
+    this._maxTemp = 32;
+    console.log(this._maxTemp);
+    this._powerSavingModeOn = false;
+  }
+  switchOn() {
+    this._maxTemp = 25;
+      console.log(this._maxTemp);
+    this._powerSavingModeOn = true;
   }
 }
